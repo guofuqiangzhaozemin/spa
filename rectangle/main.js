3d7b6e9f752441b1ea10e98cbde4d762d4155c50
zhaozemin <1963765653@qq.com>
2021-03-12 15:56:27 +0800

    数据合法性校验 update

--- rectangle/main.js
+++ rectangle/main.js
@@ -3,15 +3,19 @@ $(()=>{
       $height=$('#height'),
       $btncal=$('#calc'),
       $perimeter=$('#perimeter'),
+      $widthValidate=$("#width-validate"),
+      $heightValidate=$("#height-validate"),
       $area=$('#area');
 
 $btncal.click(()=>{
   let w=Number($width.val()),
       h=Number($height.val());
 
-
+  //validate
+if(validate($width,$widthValidate)  && validate($height,$heightValidate)){
   let p=(w+h)*2,
       a=w*h;
+
 //解决浮点舍入误差的问题
 function roundFractional(x,n){
   return Math.round(x*Math.pow(10,n))/Math.pow(10,n);
@@ -30,5 +34,32 @@ function roundFractional(x,n){
 
   $perimeter.val(roundFractional(p,Math.max(n1,n2)));
   $area.val(roundFractional(a,n1+n2));
+
+}
 })
 })
+
+function validate(input,output){
+  if(input.val()===''){
+    output.html('该字段不能为空');
+    return false;
+  }else{
+    output.html('');
+  }
+  
+  let val = Number(input.val());
+  if(isNaN(val)){
+    output.html('该字段应该为数值')
+    return false;
+  }else{
+    output.html('');
+  }
+  if(val<0){
+    output.html('该字段不能小于0');
+    return false;
+  }else{
+    output.html('');
+  }
+
+  return true;
+}
